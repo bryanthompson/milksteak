2b62165d198c48aa7b0791a755c5bfa5e6297514
Bryan Thompson <bryan.thompson@firespring.com>
2012-05-22 21:06:53 -0400

    adding page functionality

--- lib/models/page.rb
+++ lib/models/page.rb
@@ -1,4 +1,30 @@
 module Milksteak
   class Page
+    def self.list
+      milk_root = Milksteak::Admin.milk_root
+      page_dir = File.join(Milksteak::Admin.milk_root, "pages")
+      FileUtils.mkdir(milk_root) unless File.exist? milk_root
+      FileUtils.mkdir(page_dir) unless File.exist? page_dir
+      Dir.glob("#{page_dir}/*.yml")
+    end
+    
+    def self.open(name, mode = "r")
+      page_dir = File.join(Milksteak::Admin.milk_root, "pages", "#{name}.yml")
+      if File.exist?(page_dir)
+        f = File.open(page_dir, mode)
+      else
+        f = File.new(page_dir, mode)
+      end
+      f
+    end
+    
+    # reads a page into parsed params.  Pages are set up as yaml on top and
+    # content below, parsed as liquid.  jekyll style.
+    def self.read(name)
+    end
+
+    # writes params into a page.  
+    def self.write(name, params = {})
+    end  
   end
 end
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,5 +1,31 @@
 require File.join(File.dirname(__FILE__), "../spec_helper.rb")
 
 describe Milksteak::Page do
-  it "should do stuff"
+  it "should make the page dir if it doesn't exist" do
+    File.stub(:exist?).and_return false
+    FileUtils.should_receive(:mkdir).twice.and_return true
+    Milksteak::Page.list.should be_an Array
+  end
+
+  it "should get list of pages from milk_root/pages" do
+    Milksteak::Page.list.should be_an Array
+  end
+ 
+  # this utility method is used by the read and write methods as a consistent
+  # way to access yml page documents.   
+  it "should create a new page" do
+    f = File.new("/tmp/page_file", "r")
+    f.stub(:save).and_return true
+    File.should_receive(:new).with("/tmp/milk_site/pages/home.yml", "r").and_return f
+    Milksteak::Page.open("home", "r")
+  end
+  
+  it "should read a page into parsed params" do
+    f = File.new("/tmp/page_file", "r")
+    f.stub(:save).and_return true
+    
+  end
+
+  it "should write a page with contents in params"
+  
 end
