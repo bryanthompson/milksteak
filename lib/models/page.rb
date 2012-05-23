ef28fd7c0166bcec45b6e76a4629085b922a6d32
Bryan Thompson <bryan.thompson@firespring.com>
2012-05-22 22:17:31 -0400

    rw on pages work

--- lib/models/page.rb
+++ lib/models/page.rb
@@ -1,6 +1,6 @@
 module Milksteak
   class Page
-    attr_accessor :data, :content, :output
+    attr_accessor :name, :file, :data, :content, :output
 
     def self.list
       milk_root = Milksteak::Admin.milk_root
@@ -15,19 +15,39 @@ module Milksteak
       page_dir = File.join(Milksteak::Admin.milk_root, "pages", "#{name}.yml")
       f = File.exist?(page_dir) ? File.open(page_dir, mode) : File.new(page_dir, mode)
       p = self.new
-      p.read_yaml(name, f)
+      p.name = name
+      p.file = f
+      p.read_yaml
       p
     end
     
-    # writes params into a page.  
-    def self.write(name, params = {})
+    # writes to a page.  Replaces params and content, doesn't merge. 
+    def self.write(name, params = {}, content)
+      p = self.load(name, "w+")
+      p.data = params
+      p.content = content
+      p.write_yaml
+      p.file.close
+      p
+    end  
+  
+    def self.render(name)
+      p = self.load(name, "r")
+      
+      p.content
     end  
 
-    def read_yaml(name, file)
-      self.content = file.read
+    def write_yaml
+      self.file.write(YAML.dump(self.data))
+      self.file.write("---\n\n")
+      self.file.write(self.content)
+    end
+
+    def read_yaml
+      self.content = self.file.read
       begin
-        if self.content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
-          self.content = $POSTMATCH
+        if self.content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)(.*)/m
+          self.content = $3
           self.data = YAML.load($1)
         end
       rescue => e
--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -13,23 +13,30 @@ describe Milksteak::Page do
  
   # this utility method is used by the read and write methods as a consistent
   # way to access yml page documents.   
-  it "should load a new page" do
-    f = File.new("/tmp/page_file", "r")
+  it "should load a page into params" do
+    f = File.open(File.join(File.dirname(__FILE__), "../fixtures/pages/sample_page.yml"), "r")
     f.stub(:save).and_return true
     File.should_receive(:new).with("/tmp/milk_site/pages/home.yml", "r").and_return f
     page = Milksteak::Page.load("home", "r")
     page.should be_a Milksteak::Page
-    page.data.should be_a Hash
-    page.content.should == ""
+    page.data.should == { "description" => "This is just a test." }
+    page.content.should == "This is some test content.\n"
   end
   
-  it "should read a page into parsed params" do
-    f = File.open(File.join(File.dirname(__FILE__), "../fixtures/pages/sample_page.yml"), "r")
-    f.stub(:save).and_return true
-    
+  it "should write a page with contents in params" do
+    f = File.new(File.join(File.dirname(__FILE__), "../fixtures/pages/scratch_page.yml"), "w+")
+    File.should_receive(:new).with("/tmp/milk_site/pages/home.yml", "w+").and_return f
+    page = Milksteak::Page.write("home", { "description" => "TEST PAGE"}, "TEST CONTENT")
+    page.data.should == { "description" => "TEST PAGE" }
+    page.content.should == "TEST CONTENT"
   end
 
-  it "should write a page with contents in params"
+  it "should render page content" do
+    f = File.open(File.join(File.dirname(__FILE__), "../fixtures/pages/sample_page.yml"), "r")
+    File.should_receive(:new).with("/tmp/milk_site/pages/home.yml", "r").and_return f
+    content = Milksteak::Page.render("home")
+    pp content
+  end
 
   it "should route pages using middleware"
 end
