a90ef7d6fba26806d4347bc3cdcb8fe4732b0372
Bryan Thompson <bryan.thompson@firespring.com>
2012-06-01 07:18:16 -0400

    ugh, had the wrong liquid helper file the whole time. fixt.

--- lib/milksteak/liquid_helpers.rb
+++ lib/milksteak/liquid_helpers.rb
@@ -2,6 +2,20 @@ module FragmentHelper
   def fragment(name)
     Milksteak::Fragment.render(name)
   end
+
+  def hash_attribute(hash, name)
+    if hash.keys.include?(name)
+      return hash[name]
+    elsif hash.keys.include?(name.to_sym)
+      return hash[name.to_sym]
+    else
+      nil
+    end
+  end
+
+  def inspect(obj)
+    return obj.inspect
+  end
 end
 
 Liquid::Template.register_filter(FragmentHelper)
